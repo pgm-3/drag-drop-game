17cad984462cb329ec9110a5f0572cb30619b2c3
Frederick Roegiers <frederick@rogerthat.be>
2021-02-16 10:08:15 +0100

    drag and drop game halfway there

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 // IMPORTS START -->
-
+import { JSON_PATH } from "./constants.js";
+import * as dr from "./dragEvents.js";
+import { shuffle } from "./helpers.js";
 // <-- IMPORTS END
-
-// CONSTANTS
-const JSON_PATH = "PATH-TO-JSON-FILE";
 
 // APP FUNCTIONALITY
 const app = {
@@ -13,11 +12,42 @@ const app = {
     this.registerListeners();
     this.fetchData();
   },
-  cacheElements() {},
+  cacheElements() {
+    this.$animalContainer = document.querySelector("#animals");
+    this.$dropzone = document.querySelector("#dropzone");
+  },
   registerListeners() {},
-  async fetchData() {},
-  createIcons() {},
-  createDropzone() {},
+  async fetchData() {
+    const response = await fetch(JSON_PATH);
+    this.animalData = await response.json();
+
+    this.createIcons();
+    this.createDropzone();
+  },
+  createIcons() {
+    // lege animal array maken voor alle iconen
+    let iconElements = [];
+    // itereren door elk animal object
+    for (const type of this.animalData.animals) {
+      const icons = type.icons.map(
+        (icon) => `<img src="./icons/${icon}.png" alt="${icon}">`
+      );
+      iconElements.push(...icons);
+    }
+    // door elkaar schudden
+    shuffle(iconElements);
+    iconElements = iconElements.slice(0, 6);
+    this.$animalContainer.innerHTML = iconElements.join("");
+  },
+  createDropzone() {
+    let types = this.animalData.animals.map((typeObj) => typeObj.type);
+
+    const dropContainers = types.map((type) => {
+      return `<div><h2>${type}</h2></div>`;
+    });
+
+    this.$dropzone.innerHTML = dropContainers.join("");
+  },
 };
 
 // BEAM ME UP SCOTTY
